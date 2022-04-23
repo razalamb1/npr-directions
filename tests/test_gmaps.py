4c32d64ef0c0059ff057bd268d4e7e5d17229255
Raza Lamb <rhlamb95@gmail.com>
2022-04-22 20:07:08 -0400

    removing __name__

--- tests/test_gmaps.py
+++ tests/test_gmaps.py
@@ -14,7 +14,4 @@ def test_get_directions():
     with pytest.raises(OutsideUSA):
         get_directions(client, "Toronto, ON", "Chicago, IL")
     results = get_directions(client, "Chicago, IL", "Durham, NC")
-    assert results.shape == (219,3)
-
-if __name__ == "__main__":
-    test_get_directions()
+    assert results.shape == (219, 3)
