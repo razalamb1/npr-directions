310c0d9ab437b3c31c28daf6ed4946e6a916bc43
razalamb1 <rhlamb95@gmail.com>
2022-09-22 15:50:19 -0400

    updating requests.get

--- src/npr.py
+++ src/npr.py
@@ -9,7 +9,7 @@ def get_stations(npr_key: str, lon: str, lat: str) -> Tuple[str, str]:
     headers = {"Authorization": f"Bearer {npr_key}"}
     params = {"lat": lat, "lon": lon}
     url = "https://station.api.npr.org/v3/stations"
-    r = requests.get(url, headers=headers, params=params).json()["items"]
+    r = requests.get(url, headers=headers, params=params, timeout=15).json()["items"]
     if len(r) == 0:
         raise StationError("No station returned by API.")
     st_one = r[0]["attributes"]["brand"]
